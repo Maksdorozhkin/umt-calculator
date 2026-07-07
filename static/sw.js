2544cbe96350c78ed5e010ad8557cd7d1d63552e
MD <maksdorozhkin@gmail.com>
2026-07-07 08:54:51 +0300

    version update

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 // Инкрементируйте версию при КАЖДОМ изменении кода (v1 -> v2 -> v3)
-const CACHE_NAME = "umt-v1.00.4";
-const STATIC_CACHE = "umt-static-v1.00.4";
+const CACHE_NAME = "umt-v1.00.5";
+const STATIC_CACHE = "umt-static-v1.00.5";
 
 // ── Assets to precache (ВНИМАНИЕ: '/sw.js' отсюда УДАЛЕН!) ──
 const PRECACHE_URLS = [
